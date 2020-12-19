697660b9a6243d9fbf6ca1097b456a216522acf9
Andres-XL <amovilla1103@gmail.com>
2020-12-19 03:36:42 -0500

    added alignment info

--- src/Utils/Data.js
+++ src/Utils/Data.js
@@ -23,6 +23,10 @@ export const app_states = [
         app_name : 'class-info',
         app_value : 5
     },
+    {
+        app_name : 'alignment-info',
+        app_value : 6
+    },
 ]
 
 export const skills = [
@@ -302,47 +306,75 @@ export const alignments = [
     {
         name : 'Lawful Good',
         id : 0,
-        shortname : 'LG'
+        shortname : 'LG',
+        description : 'A lawful good character acts as a good person is expected or required to act. He tells the truth, ' +
+            'keeps his word, helps those in need, and speaks out against injustice. A lawful good character hates to see ' +
+            'the guilty go unpunished.'
     },
     {
         name : 'Neutral Good',
         id : 1,
-        shortname : 'NG'
+        shortname : 'NG',
+        description : 'A neutral good character does the best that a good person can do. He is devoted to helping others.'
     },
     {
         name : 'Chaotic Good',
         id : 2,
-        shortname : 'CG'
+        shortname : 'CG',
+        description : 'A chaotic good character acts as his conscience directs him with little regard for what others ' +
+            'expect of him. He makes his own way, but he\'s kind and benevolent. He believes in goodness and right but ' +
+            'has little use for laws and regulations. He hates it when people try to intimidate others and tell them what ' +
+            'to do. He follows his own moral compass, which, although good, may not agree with that of society.'
     },
     {
         name : 'Lawful Neutral',
         id : 3,
-        shortname : 'LN'
+        shortname : 'LN',
+        description : 'A lawful neutral character acts as law, tradition, or a personal code directs her. Order and ' +
+            'organization are paramount to her. She may believe in personal order and live by a code or standard, or she ' +
+            'may believe in order for all and favor a strong, organized government.'
     },
     {
         name : 'True Neutral',
         id : 4,
-        shortname : 'N'
+        shortname : 'N',
+        description : 'A neutral character does what seems to be a good idea. She doesn\'t feel strongly one way or the ' +
+            'other when it comes to good vs. evil or law vs. chaos. Such a character thinks of good as better than evil - after all,' +
+            ' she would rather have good neighbors and rulers than evil ones. Still, she\'s not personally committed to ' +
+            'upholding good in any abstract or universal way.'
     },
     {
         name : 'Chaotic Neutral',
         id : 5,
-        shortname : 'CN'
+        shortname : 'CN',
+        description : 'A chaotic neutral character follows his whims. He is an individualist first and last. He values ' +
+            'his own liberty but doesn\'t strive to protect others\' freedom. He avoids authority, resents restrictions, ' +
+            'and challenges traditions.'
     },
     {
         name : 'Lawful Evil',
         id : 6,
-        shortname : 'LE'
+        shortname : 'LE',
+        description : 'A lawful evil villain methodically takes what he wants within the limits of his code of conduct ' +
+            'without regard for whom it hurts. He cares about tradition, loyalty, and order but not about freedom, ' +
+            'dignity, or life. He plays by the rules but without mercy or compassion.'
     },
     {
         name : 'Neutral Evil',
         id : 7,
-        shortname : 'NE'
+        shortname : 'NE',
+        description : 'A neutral evil villain does whatever she can get away with. She is out for herself, pure and simple. ' +
+            'She sheds no tears for those she kills, whether for profit, sport, or convenience. She has no love of order ' +
+            'and holds no illusion that following laws, traditions, or codes would make her any better or more noble. ' +
+            'On the other hand, she doesn\'t have the restless nature or love of conflict that a chaotic evil villain has.'
     },
     {
         name : 'Chaotic Evil',
         id : 8,
-        shortname : 'CE'
+        shortname : 'CE',
+        description : 'A chaotic evil character does whatever his greed, hatred, and lust for destruction drive him to do. ' +
+            'He is hot-tempered, vicious, arbitrarily violent, and unpredictable. If he is simply out for whatever he can ' +
+            'get, he is ruthless and brutal. If he is committed to the spread of evil and chaos, he is even worse.'
     },
 ]
 
@@ -542,6 +574,6 @@ export const classes = [
             martial : {}
         }
     },
-    
-    
+
+
 ]
