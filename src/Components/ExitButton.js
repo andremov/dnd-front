1af34d683d78236722e74aae8835b884da3465b4
Andres-XL <amovilla1103@gmail.com>
2021-01-02 18:08:45 -0500

    fix close btn

--- src/Components/ExitButton.js
+++ src/Components/ExitButton.js
@@ -5,21 +5,21 @@ export function ExitButton( { canExit = true, eventCallback, card_id } ) {
         return <Fragment />
     }
     
-    return <div
-        className={'exit-btn-wrapper'}
-    >
-        <div
-            className={'exit-btn'}
-            onClick={
-                () => eventCallback(
-                    {
-                        action : 'remove',
-                        card_id : card_id
-                    }
-                )
-            }
-        >
-            <div>X</div>
-        </div>
-    </div>
+    return (
+        // <div className={'exit-btn-wrapper'}>
+            <div
+                className={'exit-btn'}
+                onClick={
+                    () => eventCallback(
+                        {
+                            action : 'remove',
+                            card_id : card_id
+                        }
+                    )
+                }
+            >
+                <div>X</div>
+            </div>
+        // </div>
+    )
 }
